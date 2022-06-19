c3e0fe7b6e9b13bd92f5f7fafe815bc22868d4a4
ignorantMonkey <tero.s.heikkinen1@gmail.com>
2022-06-19 03:05:58 +0300

    Adding first draft of testing different parsings

--- program.py
+++ program.py
@@ -1,9 +1,45 @@
 import asyncio
+import requests
+
+from pyquery import PyQuery
+from lxml.html import parse
+from lxml.html import fromstring
+
+host = 'https://news.ycombinator.com/'
+page = 'show'
+URL = host + page
+
+def requ():
+    response = requests.get(URL)
+    open('testi.txt','wb').write(response.content)
+
+def req_pyquery():
+    data_html = open('testi.txt', 'r').read()
+    obj = PyQuery(data_html)
+    print (obj('head').text())
+    print (obj('td').text())
+
+def req_etree():
+    info = parse('testi.txt').getroot()
+    for x in info.cssselect('td'):
+        print(x.text_content())
+
+import urllib
+def req_etree2():
+    #content = urllib.urlopen(URL).read()
+    content = open('testi.txt', 'r').read()
+    doc = fromstring(content)
+    doc.make_links_absolute(URL)
+    print("DOC: " + doc.text_content())
+    for x in doc.find_class('subtext'):
+        print(x.text_content())
 
 async def main():
     print('Hello ...')
     await asyncio.sleep(1)
-    print('... World!')
+    print('... World! ' + URL)
 
-asyncio.run(main())
-
+#asyncio.run(main())
+#req_pyquery()
+#req_etree()
+req_etree2()
